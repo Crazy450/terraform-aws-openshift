b8a667a9d3a8bcb1f0778fdd9e2a8deb81d5c29f
Crazy450 <filipe@infosantos.com>
2018-02-09 15:44:24 -0500

    fea - cleanup

--- install-from-bastion.sh
+++ install-from-bastion.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Elevate priviledges, retaining the environment.
-sudo su - 
-
 # Install dev tools and Ansible 2.2
-yum install -y "@Development Tools" python2-pip openssl-devel python-devel gcc libffi-devel vim
-pip install -Iv ansible==2.3.0.0
+sudo yum install -y "@Development Tools" python2-pip openssl-devel python-devel gcc libffi-devel vim
+sudo pip install -Iv ansible==2.3.0.0
 
 # Clone the openshift-ansible repo, which contains the installer.
 git clone -b release-3.6 https://github.com/openshift/openshift-ansible
 
 # Run the playbook.
-ANSIBLE_HOST_KEY_CHECKING=False /usr/local/bin/ansible-playbook -i ./inventory.cfg ./openshift-ansible/playbooks/byo/config.yml # uncomment for verbose! -vvv
+#/usr/local/bin/ansible-playbook -i ./inventory.cfg ./openshift-ansible/playbooks/byo/config.yml # uncomment for verbose! -vvv
 
 # If needed, uninstall with the below:
 # ansible-playbook playbooks/adhoc/uninstall.yml
