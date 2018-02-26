65244690db012eb4ef3cfd61982b4a650a167075
Filipe Bento Dos Santos <filipe.bentodossantos@bnc.ca>
2018-02-26 18:26:33 -0500

    bug - Fix typo error
    Typo

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 
 variable "profile" {
   description = "Profile configured using aws-cli"
-  default = "ts_aws_work"
+  default = "$ProfileName"
 }
 //  The public key to use for SSH access.
 variable "public_key_path" {
